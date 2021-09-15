fcc6f9c444d93ac9b3997bbe4c8cde8d296d0699
Lucas Claro <lucasbrclaro@gmail.com>
2021-09-15 02:56:25 -0300

    correcao pos video

--- TratarDados.py
+++ TratarDados.py
@@ -4,9 +4,6 @@ pokemons = pd.read_csv("main.csv")
 
 pokemonsDanos = pd.read_csv("serebii.csv")
 
-pokemons[['Ndex','pokemon_name']] = pokemons[['Ndex','pokemon_name']].astype('object')
-pokemonsDanos[['Ndex','pokemon_name']] = pokemonsDanos[['Ndex','pokemon_name']].astype('object')
-
 novoDF = pd.merge(pokemons, pokemonsDanos, how='outer')
 
 print(novoDF.tail())
